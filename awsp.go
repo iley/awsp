c8c3b92000f6eae737084ef86e579a66fa5b1a97
Ilya Strukov <ilya.strukov@aid-driving.eu>
2019-05-25 10:05:07 +0200

    Command-line parsing

--- awsp.go
+++ awsp.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	// "gopkg.in/ini.v1"
 )
 
+var credentialsPath = flag.String("credentials", "~/.aws/credentials", "path to the AWS credentials file")
+var configPath = flag.String("config", "~/.aws/config", "path to the AWS config file")
+var proflie = flag.String("set", "", "profile to set as default")
+
 func main() {
-	fmt.Println("hello world")
+	flag.Parse()
+
+	var err error
+	if *proflie != "" {
+		err = setProfile()
+	} else {
+		err = getProfiles()
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
+func getProfiles() error {
+	fmt.Println("getProfiles()")
+	return nil
+}
+
+func setProfile() error {
+	fmt.Println("setProfile()")
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/iley/awsp
 
 go 1.12
+
+require gopkg.in/ini.v1 v1.42.0
