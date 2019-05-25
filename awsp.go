50bd71e0abf265e5742eb675ea6d4d67cb274009
Ilya Strukov <ilya.strukov@aid-driving.eu>
2019-05-25 10:18:42 +0200

    Read profiles from the config

--- awsp.go
+++ awsp.go
@@ -3,22 +3,30 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
-	// "gopkg.in/ini.v1"
+	"path"
+
+	"gopkg.in/ini.v1"
 )
 
-var credentialsPath = flag.String("credentials", "~/.aws/credentials", "path to the AWS credentials file")
-var configPath = flag.String("config", "~/.aws/config", "path to the AWS config file")
-var proflie = flag.String("set", "", "profile to set as default")
-
 func main() {
+	homeDir := os.Getenv("HOME")
+	if homeDir == "" {
+		log.Fatalf("$HOME not set")
+	}
+
+	credentialsPath := flag.String("credentials", path.Join(homeDir, ".aws/credentials"), "path to the AWS credentials file")
+	_ = flag.String("config", path.Join(homeDir, ".aws/config"), "path to the AWS config file")
+	proflie := flag.String("set", "", "profile to set as default")
+
 	flag.Parse()
 
 	var err error
 	if *proflie != "" {
 		err = setProfile()
 	} else {
-		err = getProfiles()
+		err = getProfiles(*credentialsPath)
 	}
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
@@ -26,8 +34,34 @@ func main() {
 	}
 }
 
-func getProfiles() error {
-	fmt.Println("getProfiles()")
+func getProfiles(credentialsPath string) error {
+	cfg, err := ini.Load(credentialsPath)
+	if err != nil {
+		return err
+	}
+
+	defaultSec, err := cfg.GetSection("default") // not to be confused with DEFAULT which is top-level INI section
+	if err != nil {
+		return err
+	}
+	if !defaultSec.HasKey("aws_access_key_id") {
+		return fmt.Errorf("aws_access_key_id not found in \"default\" section of %s", credentialsPath)
+	}
+
+	defaultKey := defaultSec.Key("aws_access_key_id").Value()
+
+	for _, sec := range cfg.Sections() {
+		if sec.Name() == "DEFAULT" || sec.Name() == "default" {
+			continue
+		}
+		key := sec.Key("aws_access_key_id").Value()
+		if key != "" && key == defaultKey {
+			fmt.Printf("* %s\n", sec.Name())
+		} else {
+			fmt.Printf("  %s\n", sec.Name())
+		}
+	}
+
 	return nil
 }
 
